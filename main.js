7ad9920bf2789bd7f2d9b00e3e548b70fb0afba4
Michael Hernandez <gammabeta010@gmail.com>
2020-02-27 21:24:20 +0100

    Finito il gioco

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-$(".hamburger-menu").on( {
+
+$(".fa-bars").on( {
     click: function() {
         $(".hamburger-menu").show();
+}});
+$(".fa-times").on( {
+    click: function() {
+        $(".hamburger-menu").hide();
 }});
